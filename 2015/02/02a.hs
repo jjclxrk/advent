9623ebf5eee97f6d46ab8cdab4bb5679ac3d46bc
Joshua Clark <jjclxrk@gmail.com>
2020-06-12 23:02:56 +1000

    2015, day 2

---
+++ 2015/02/02a.hs
@@ -0,0 +1,32 @@
+-- Day 2: I Was Told There Would Be No Math, Part One
+
+-- The elves are running low on wrapping paper, and so they need to submit an 
+-- order for more. They have a list of the dimensions (length l, width w, and 
+-- height h) of each present, and only want to order exactly as much as they 
+-- need.
+
+-- Fortunately, every present is a box (a perfect right rectangular prism), 
+-- which makes calculating the required wrapping paper for each gift a little 
+-- easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. 
+-- The elves also need a little extra paper for each present: the area of the 
+-- smallest side.
+
+-- All numbers in the elves' list are in feet. How many total square feet of 
+-- wrapping paper should they order?
+
+import Data.List.Split
+
+main = readFile "input.txt" >>= print . solve . parse
+
+-- input dimensions are in the form "LxWxH" where L, W, H are the length, width
+-- and height of the presents
+parse :: String -> [[Int]]
+parse = map (map read . splitOn "x") . lines
+
+
+solve :: [[Int]] -> Int
+solve = sum . map surfaceArea
+    where
+      sides [l,w,h] = [l*w, w*h, h*l]
+      surfaceArea   = (\ss -> 2 * sum ss + minimum ss) . sides 
+
---
+++ 2015/02/02b.hs
@@ -0,0 +1,31 @@
+-- Day 2: I Was Told There Would Be No Math, Part TWO
+
+-- The elves are also running low on ribbon. Ribbon is all the same width, so 
+-- they only have to worry about the length they need to order, which they 
+-- would again like to be exact.
+
+-- The ribbon required to wrap a present is the shortest distance around its 
+-- sides, or the smallest perimeter of any one face. Each present also requires 
+-- a bow made out of ribbon as well; the feet of ribbon required for the 
+-- perfect bow is equal to the cubic feet of volume of the present. Don't ask 
+-- how they tie the bow, though; they'll never tell.
+
+-- How many total feet of ribbon should they order?
+
+import Data.List
+import Data.List.Split
+
+main = readFile "input.txt" >>= print . solve . parse
+
+-- input dimensions are in the form "LxWxH" where L, W, H are the length, width
+-- and height of the presents
+parse :: String -> [[Int]]
+parse = map (map read . splitOn "x") . lines
+
+solve :: [[Int]] -> Int
+solve = sum . map ribbon
+    where
+      minPerimeter = (*2) . sum . init . sort
+      ribbon       = \ss -> product ss + minPerimeter ss
+
+
